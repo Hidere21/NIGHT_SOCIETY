69210425e8d3fd75be17aadee76a5285e13a4b07
Mayer <andres.20.chaves@gmail.com>
2023-06-16 18:48:54 -0500

    [add] add partial work

--- NIGHT_SOCIETY/src/app/routes/amigos/amigos.module.ts
+++ NIGHT_SOCIETY/src/app/routes/amigos/amigos.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './page/home/home.component';
+import { FriendsComponent } from './page/friends/friends.component';
+import { ContactsComponent } from './page/contacts/contacts.component';
+import { LandingFriendsComponent } from './page/landing-friends/landing-friends.component';
+import { CardFriendComponent } from './components/card-friend/card-friend.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { CardContactComponent } from './components/card-contact/card-contact.component';
 
 @NgModule({
   declarations: [
+    ContactsComponent,
+    FriendsComponent,
     HomeComponent,
+    LandingFriendsComponent,
+    CardFriendComponent,
+    SearchInputComponent,
+    CardContactComponent
   ],
   imports: [
     CommonModule,
---
+++ NIGHT_SOCIETY/src/app/routes/amigos/components/card-contact/card-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-contact',
+  templateUrl: './card-contact.component.html',
+  styles: []
+})
+export class CardContactComponent {
+
+}
