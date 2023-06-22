4cae95acbc9434b93f80f5841909e197db441a82
DubanStevenCastilloMendoza <estivencastmen@gmail.com>
2023-06-22 03:20:41 +0200

    conectando la landing page con el register

--- NIGHT_SOCIETY/src/app/app-routing.module.ts
+++ NIGHT_SOCIETY/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LaddignPageComponent } from './routes/laddign-page/laddign-page.component';
 import { RegisterComponent } from './routes/register/register.component';
+import { PoliticasComponent } from './routes/politicas/politicas.component';
 
 const routes: Routes = [
   {path: "laddig", component: LaddignPageComponent},
-  {path: "register", component: RegisterComponent}
+  {path: "register", component: RegisterComponent},
+  {path: "politicas", component: PoliticasComponent}
 ];
 
 @NgModule({
