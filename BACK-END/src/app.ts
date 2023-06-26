31e7e5ee3fdd5a12c5c4cb020480e942ec483795
Mayer <andres.20.chaves@gmail.com>
2023-06-26 18:41:56 -0500

    [add] add multer

--- BACK-END/src/app.ts
+++ BACK-END/src/app.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import morgan from "morgan";
 import dataBase from "./config/database";
 import apiRoutes from "./routes/index"
+import multer, { Multer } from 'multer';
+import path from 'path'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +14,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan("dev"));
+
+// Configuración de Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, `${__dirname}/../uploads`); // Ruta donde se guardarán los archivos subidos
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname); // Nombre de archivo único
+    }
+  });
+
+const upload: Multer = multer({ storage });
 
 // Rutas
 app.use("/api", apiRoutes);
