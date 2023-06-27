b80966ea3e090e97d14e8baf60e8e0db779563fb
Maksym Buzyak <PaulaToro99>
2023-06-26 20:47:54 -0500

    merge

--- NIGHT_SOCIETY/src/app/app-routing.module.ts
+++ NIGHT_SOCIETY/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LaddignPageComponent } from './routes/laddign-page/laddign-page.component';
-import { HomeComponent } from './routes/home/home.component';
-import { LoginComponent } from './routes/login/login.component';
+import { CalificarComponent } from './routes/calificar/calificar.component';
+import { PoliticasComponent } from './routes/politicas/politicas.component';
+import { CrearEventosComponent } from './routes/crear-eventos/crear-eventos.component';
+import { EventosComponent } from './routes/eventos/eventos.component';
 
 const routes: Routes = [
-  {path: "laddig", component: LaddignPageComponent},
-  {path: "home", component: HomeComponent},
-  {path: "login", component: LoginComponent}
+  { path: 'laddig', component: LaddignPageComponent },
+  { path: 'calificar', component: CalificarComponent },
+  { path: 'politicas', component: PoliticasComponent },
+  { path: 'laddig', component: LaddignPageComponent },
+  { path: 'createEvent', component: CrearEventosComponent },
+  { path: 'events', component: EventosComponent },
 ];
 
 @NgModule({
