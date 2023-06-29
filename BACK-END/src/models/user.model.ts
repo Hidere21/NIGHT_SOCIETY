34a4604b5a5d5dd04c19143b9f7c943ae17cec5a
Mayer <andres.20.chaves@gmail.com>
2023-06-28 21:48:57 -0500

    [add] mergin branch

--- BACK-END/src/models/user.model.ts
+++ BACK-END/src/models/user.model.ts
@@ -21,7 +21,7 @@ const userSchema = new Schema<User>(
         },
       ],
     },
-    studyOrWork: { type: String, required: true },
+    studyOrWork: { type: String, required: false },
     userImage: { type: String, default: "" },
   },
   {
--- NIGHT_SOCIETY/src/app/app.component.ts
+++ NIGHT_SOCIETY/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NIGHT_SOCIETY';
+
+  ngOnInit(){
+    this.nav()
+  }
+
+  nav(){
+    let l: any = window.location.href
+    l = l.split('/')
+    console.log(l)
+    return l[3] === "laddig" || l[3] === "Login" || l[3] === "register"  ? false : true
+  }
 }
